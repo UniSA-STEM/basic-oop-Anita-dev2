d1b02531ada28cd267aca9519dd414bc00dafa38
Anita-dev2 <maray160@mymail.unisa.edu.au>
2025-10-12 15:55:10 +1100

    Created Asset constructor, getters, encryption and decryption, string method

--- Asset.py
+++ Asset.py
@@ -6,3 +6,32 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Asset:
+    def __init__(self, name, description):
+        self.__name = name
+        self.__description = description
+        self.__encrypted =  False
+
+    def get_name(self):
+        return self.__name
+
+    def get_description(self):
+        return self.__description
+
+    def get_encrypted(self):
+        return self.__encrypted
+
+    def encrypt(self):
+        self.__encrypted = True
+
+    def decrypt(self):
+        self.__encrypted = False
+
+    def __str__(self):
+        if self.__encrypted == True:
+            return_string = f"<{self.__name}>:<{self.__description}>[Encrypted]"
+        else:
+            return_string = f"<{self.__name}>:<{self.__description}>"
+
+        return return_string
