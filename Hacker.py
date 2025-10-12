694890faf110b703cbdacfce0becba2636bf6ac2
Anita-dev1 <anitamaratheftis@gmail.com>
2025-10-12 11:35:05 +1100

    Created Hacker class and acquire_rig method

--- Hacker.py
+++ Hacker.py
@@ -6,3 +6,28 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Hacker:
+    def __init__(self, rig):
+        self.name = "xxx_(rYPT"
+        self.crypto_token = 1
+        self.rig = rig
+        self.trace_level = 0
+
+
+    def acquire_rig(self, num):
+        if num > 0:
+            self.rig = self.rig + 1
+            self.crypto_token = self.crypto_token - 1
+            print("You have activated a new rig")
+        else:
+            print("You do not have enough tokens to acquire a new rig. Try again later")
+
+
+    def trace(self):
+        pass
+
+
+
+hacker = Hacker(0)
+hacker.acquire_rig(1)
