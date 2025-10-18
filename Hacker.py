6200c1c0d4c8cc57f9d54f7c53dd307bc22b4b27
Anita-dev2 <maray160@mymail.unisa.edu.au>
2025-10-18 17:30:38 +1100

    Small modifications made to numerous methods, comments and docstrings added

--- Hacker.py
+++ Hacker.py
@@ -6,7 +6,6 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
-import random
 from Asset import Asset
 from Rig import Rig
 
@@ -15,30 +14,66 @@ class Hacker:
     def __init__(self, name):
         self.__name = name
         self.__inventory = []
-        crypto_token1 = Asset("CryptoToken", "CryptoToken")
-        self.__inventory.append(crypto_token1)
+        self.__crypto_token1 = Asset("CryptoToken", "This is used to acquire and repair rigs")
+        self.__inventory.append(self.__crypto_token1)
+        self.__hardware_patch = Asset("Hardware Patch", "This is used to upgrade rigs")
+        self.__inventory.append(self.__hardware_patch)
+        self.__security_chip = Asset("Security Chip", "This is used to encrypt or decrypt assets")
+        self.__inventory.append(self.__security_chip)
         self.__rig = 0
         self.__trace_level = 0
 
 
     def acquire_rig(self, rig_name):
-        for item in self.__inventory:
-            if item.get_name() == "CryptoToken":
-                self.__rig = Rig(rig_name)
-                self.__inventory.remove(item)
-                print("You have activated a new rig")
-            else:
+        """
+        This method allows the Hacker to acquire a rig provided that the hacker
+        has a CryptoToken in their inventory.
+        :param rig_name:
+        :return: None
+        """
+        if self.get_trace() < 5:
+            found = False
+            for item in self.__inventory:
+                # Check for CryptoToken in inventory, print to user whether action was successful or not
+                if item.get_name() == "CryptoToken":
+                    self.__rig = Rig(rig_name)
+                    self.__inventory.remove(item)
+                    print("You have activated a new rig")
+                    found = True
+                    break
+            if not found:
                 print("You do not have enough tokens to acquire a new rig")
+        else:
+            print("Your trace level is too high!")
 
     def encrypt_assets(self, asset_name):
-        for item in self.__rig.get_storage():
-            if item.get_name() == "Security Chip":
-                for item_to_encrypt in self.__rig.get_storage():
-                    if item_to_encrypt.get_name() == asset_name and not item_to_encrypt.is_encrypted():
-                        item_to_encrypt.encrypt()
-                        self.__rig.consume_asset(item)
-                        break
-        print(f"There is no unencrypted {asset_name}(s)")
+        """
+        This method encrypts an asset provided that the hacker has a security chip in their inventory
+        :param asset_name:
+        :return:
+        """
+        # Check for Security Chip in inventory, if yes, proceeds to checks for if there is a match in asset and whether the
+        # asset is already encrypted. If it is not, the method encrypts the asset and consumes the security chip
+        if self.get_trace() < 5:
+            found_asset = False
+            sec_chip = False
+            for item in self.__inventory:
+                if item.get_name() == "Security Chip":
+                    sec_chip = True
+                    for item_to_encrypt in self.__rig.get_storage():
+                        if item_to_encrypt.get_name() == asset_name and not item_to_encrypt.is_encrypted():
+                            found_asset = True
+                            item_to_encrypt.encrypt()
+                            self.__inventory.remove(item)
+                            break
+                if found_asset:
+                    break
+            if not sec_chip:
+                print("You do not have any security chips.")
+            elif not found_asset:
+                print(f"There is no unencrypted {asset_name}(s)")
+        else:
+            print("Your trace level is too high!")
 
     def decrypt_assets(self, asset_name):
         """
@@ -71,45 +106,39 @@ class Hacker:
 
 
     def get_rig(self):
+        """
+        Getter for self.__rig
+        :return:
+        """
         return self.__rig
 
     def launch_data_spike(self, target_rig):
-        for item in self.__rig.get_storage():
-             if item.get_name() == "Data Spike":
-                target_rig.take_damage()
-                self.__rig.consume_asset(item)
-                break
-
-        if target_rig.is_broken():
+        """
+        This method launches data spikes provided that certain criteria is met. First, the trace level of the hacker is checked.
+        If the trace level is less than 5 (the max), the next check is that the hacker has a Data Spike, if yes, this is removed from the inventory.
+        If the target rig is broken, the hacker must have a Removable Drive to be able to extract assets from the target rig.
+        If the target assets are not encrypted, the assets are added into the rig storage and removed from the target rig storage.
+        :param target_rig:
+        :return:
+        """
+        if self.get_trace() < 5:
             for item in self.__rig.get_storage():
-                if item.get_name() == "Removable Drive":
+                 if item.get_name() == "Data Spike":
+                    target_rig.take_damage()
                     self.__rig.consume_asset(item)
-                    '''storage_size = len(target_rig.get_storage()) - 1
-                    for index in range(storage_size, -1, -1):
-                        if not target_rig.get_storage()[index].is_encrypted():
-                            self.__rig.store_asset(target_rig.get_storage()[index])
-                            target_rig.consume_asset(target_rig.get_storage()[index])'''
-                    for item2 in target_rig.get_storage()[:]:
-                        if not item2.is_encrypted():
-                            self.__rig.store_asset(item2)
-                            target_rig.consume_asset(item2)
                     break
 
-
-
-    def add_asset(self, asset_name):
-        if asset_name == "Data Spike":
-            asset = Asset("Data Spike", "CryptoToken")
-            self.__inventory.append(asset)
-        elif asset_name == "patch":
-            asset = Asset("patch", "CryptoToken")
-            self.__inventory.append(asset)
-        elif asset_name == "token":
-            asset = Asset("token", "CryptoToken")
-            self.__inventory.append(asset)
-        elif asset_name == "chip":
-            asset = Asset("chip", "CryptoToken")
-            self.__inventory.append(asset)
+            if target_rig.is_broken():
+                for item in self.__rig.get_storage():
+                    if item.get_name() == "Removable Drive":
+                        self.__rig.consume_asset(item)
+                        for item2 in target_rig.get_storage()[:]:
+                            if not item2.is_encrypted():
+                                self.__inventory.append(item2)
+                                target_rig.consume_asset(item2)
+                        break
+        else:
+            print("Your trace level is too high!")
 
     def get_inventory(self):
         return self.__inventory
@@ -129,18 +158,24 @@ class Hacker:
                     self.__inventory.remove(item)
 
     def repair_my_rig(self):
-        if self.__crypto_token > 0:
-            if self.__rig.rig_repair():
-                self.__crypto_token = self.__crypto_token - 1
+        for item in self.__inventory:
+            if item.get_name() == "CryptoToken":
+                if self.__rig.rig_repair():
+                    self.__inventory.remove(item)
+                    print("Repair complete")
+                else:
+                    print("No repair needed")
             else:
-                print("No repair needed")
-        else:
-            print("Insufficient funds to repair")
+                print("Insufficient funds to repair")
+
 
     def upgrade_my_rig(self):
-        # Needs hardware patch, increases rig level, storage size and decreases battle
-        # need rig and hardware patch to execute
-        pass
+        for item in self.__inventory:
+            if item.get_name() == "Hardware Patch":
+                self.__rig.increment_upgrade_level()
+                self.__inventory.remove(item)
+                break
+
 
     def transfer_all_assets(self, list1, list2):
         for item in list1[:]:
